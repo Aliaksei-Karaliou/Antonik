457a49c7838c1fa5028693f28ebd0617901fd445
Aliaksei Karaliou <alexeykoroliov@gmail.com>
2017-02-15 23:41:00 +0300

    some changes

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 var input = document.getElementById("input");
+var temp = null;
 
 input.oninput = function () {
     if (!isNumber(input.value)) {
+        var deleted = input.value.substring(input.value.length - 1);
         input.value = input.value.substring(0, input.value.length - 1);
+        if (deleted == '+') {
+            onPlus();
+        }
     }
 };
 
-function numberOnClick(number) {
-    var value = input.value + number.toString();
+function onNumberClick(number) {
+    var value = input.value + number.toString().replace(/^0+/, '');
+    alert(value);
     if (isNumber(value)) {
         input.value = value;
     }
+}
+
+function onPlus() {
+    alert("Plus");
 }
 
 function isNumber(possibleNumber) {
