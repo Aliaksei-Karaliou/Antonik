7dfa1f35e6a67bcc0704ae1bcc58422258136ca4
Aliaksei Karaliou <alexeykoroliov@gmail.com>
2017-02-15 19:24:46 +0300

    Final commit

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
 var button = document.getElementById("result");
 
 button.onclick = function () {
-    try {
-        var argument1 = parseFloat(document.getElementById("arg1").value);
+
+    var argument1, argument2;
+    if (isNumber(document.getElementById("arg1").value)) {
+        argument1 = parseFloat(document.getElementById("arg1").value);
     }
-    catch (error) {
-        alert(button.value);
+    else {
+        alert("Argument 1 is not number");
         return;
     }
-    var sign = document.getElementById("arg1").value;
-    var argument2 = parseFloat(document.getElementById("arg2").value);
+    if (isNumber(document.getElementById("arg2").value)) {
+        argument2 = parseFloat(document.getElementById("arg2").value);
+    }
+    else {
+        alert("Argument 2 is not number");
+        return;
+    }
 
-    button.value = argument1 + argument2;
+    var sign = document.getElementById("sign").value;
+    switch (sign) {
+        case "+":
+            button.value = makeNumberGreatAgain(argument1 + argument2);
+            break;
+        case "-":
+            button.value = makeNumberGreatAgain(argument1 - argument2);
+            break;
+        case "*":
+            button.value = makeNumberGreatAgain(argument1 * argument2);
+            break;
+        case "/":
+            button.value = makeNumberGreatAgain(argument1 / argument2);
+            break;
+        case "%":
+            button.value = argument1 % argument2;
+            break;
+        default:
+            alert("Sign is not valid");
+            break;
+    }
 
 };
+
+function isNumber(possibleNumber) {
+    return possibleNumber === parseFloat(possibleNumber).toString();
+}
+
+function makeNumberGreatAgain(number) {
+    return parseFloat(number).toFixed(13).replace(/0+$/, '');
+}
+
+
